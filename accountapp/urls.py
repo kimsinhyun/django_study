8993c857c9cf271f253c7dca23edec78eeea6c91
kimsinhyun <ksh415622@naver.com>
2022-01-16 00:31:35 +0900

    first commit

---
+++ accountapp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from accountapp.views import hello_world
+
+#이렇게 써놓는게 좋다.
+app_name = "accountapp"
+
+
+urlpatterns = [
+    # route, view, name
+    path('hello_world/', hello_world,  name='hello_world'),
+]
