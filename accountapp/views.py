347b13d450b5723f10f8fd2ab7ceeccd6ef5d40b
kimsinhyun <ksh415622@naver.com>
2022-01-16 21:11:26 +0900

    save datas to DB from input tag

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from accountapp.models import HelloWorld
 
 # Create your views here.
 
 def hello_world(request):
     if request.method == "POST":
-        return render(request, 'accountapp/helloworld.html', context={'text': 'POST method!!'})
+
+        temp = request.POST.get('hello_world_input')
+
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+
+        return render(request, 'accountapp/helloworld.html', context={ 'hello_world_output' : new_hello_world })
     elif request.method == "GET":
         return render(request, 'accountapp/helloworld.html', context={'text': 'GET method!!'})
     
