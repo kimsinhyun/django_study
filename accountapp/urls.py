6eca8ac2fc3271594d0e8e7bd51e99aabb62e2c3
kimsinhyun <ksh415622@naver.com>
2022-01-21 00:18:35 +0900

    simple implement of signup & concept of CreateView (and reverse_lazy)

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from accountapp.views import hello_world
+from accountapp.views import hello_world,AccountCreateView
 
 #이렇게 써놓는게 좋다.
-app_name = "account"
+
+app_name = "accountapp"
 
 
 urlpatterns = [
     # route, view, name
-    path('hello_world/', hello_world,  name='hello_world'),
+    path('hello_world/', hello_world,  name='hello_world'), #함수 기반 view는 그냥 함수명을 가져오면 됨
+    path('create/', AccountCreateView.as_view(), name='create'), #클래스 기반view는 .as_view()를 가져와야 됨
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from accountapp.models import HelloWorld
-from django.urls import reverse
-
+from django.urls import reverse, reverse_lazy
 
 # Create your views here.
+from django.views.generic import CreateView
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
+#FBV  (함수기반 view)
 def hello_world(request):
     if request.method == "POST":
 
@@ -16,10 +19,23 @@ def hello_world(request):
         new_hello_world.save()
 
         hello_world_list =  HelloWorld.objects.all()
-
-        return HttpResponseRedirect(reverse('account:hello_world'))
+        #reserse는 특정 url로 리다이렉트해주는 함수
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
     elif request.method == "GET":
         hello_world_list =  HelloWorld.objects.all()
 
-        return render(request, 'accountapp/helloworld.html', context={ 'hello_world_list' : hello_world_list })
-    
+        return render(request, 'accountapp/hello_world.html', context={ 'hello_world_list' : hello_world_list })
+
+#CBV 클래스 기반 View
+class AccountCreateView(CreateView):
+    model = User
+    #form_class -> CreateView에서 오버라이딩 ; UserCreationForm은 import해서 사용할 수 있는 간단한 내장 form
+    form_class = UserCreationForm
+
+    #성공했을 때 reverse_lazy를 사용해서 보여줄 화면에 연결시켜줌
+    #reverse와 reverse_lazy차이 -> reverse_lazy는 곧바로 처리되는게 아니라 나중에 해당 변수가 직접 접근되었을 때 실행 됨
+    #따라서 success할 때 동작시킬 것이기 때문에 lazy로 정의 
+    #reverse_lazy   ->  클래스형 view에서 사용
+    #reverse        ->  함수형 view에서 사용
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/create.html'   #회원가입할 때 보일 HTML
