7c057d5f215800c6495a8b956b0f5c2e4e1db7e8
kimsinhyun <ksh415622@naver.com>
2022-01-22 14:12:17 +0900

    login logout UI & redirect setting

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from accountapp.views import hello_world,AccountCreateView
 
@@ -9,5 +10,9 @@ app_name = "accountapp"
 urlpatterns = [
     # route, view, name
     path('hello_world/', hello_world,  name='hello_world'), #함수 기반 view는 그냥 함수명을 가져오면 됨
+
+    #login logout은 내장 클래스를 사용하기 때문에 이렇게 해주면 된다.
+    path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='accountapp/login.html'), name='logout'),
     path('create/', AccountCreateView.as_view(), name='create'), #클래스 기반view는 .as_view()를 가져와야 됨
 ]
